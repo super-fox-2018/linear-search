03fce4685eac68c7a558aaf8a4164a8c34fb0afa
Rin Ng <Rin08.ng@gmail.com>
2018-05-01 17:41:32 +0700

    Added code for linear search

--- linear_search.js
+++ linear_search.js
@@ -3,7 +3,13 @@
 // Release 0
 console.log("Linear Search")
 let linearSearch = (target, values) => {
-  //write your code here
+    for(var i = 0; i < values.length; i++){
+      if(target === values[i]){
+        return i;
+      }
+    }
+    return -1;
+
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
@@ -16,13 +22,20 @@ console.log(linearSearch(9, random_numbers));
 // Release 1
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
-  //write your code here
+  var arr = [];
+   for(var i = 0; i < values.length; i++){
+     if(target === values[i]){
+       arr.push(i);
+     }
+   }
+   return arr;
 }
 
 let banana_arr = "banana".split("");
 // ["b", "a", "n", "a", "n", "a"]
 console.log(globalLinearSearch("a", banana_arr));
 // [ 1, 3, 5 ]
+
 
 module.exports = {
   linearSearch,
