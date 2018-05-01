5d54e3eff1d48da6b84ef6d56d0630b9695632f3
Michael Cangcianno <michaelcangcianno@Michaels-MacBook-Pro.local>
2018-05-01 16:38:41 +0700

    finished linear search problem

--- linear_search.js
+++ linear_search.js
@@ -1,12 +1,17 @@
-'use strict'
+'use strict';
 
 // Release 0
-console.log("Linear Search")
+console.log('Linear Search');
 let linearSearch = (target, values) => {
-  //write your code here
-}
+  for (var i = 0; i < values.length; i++) {
+    if (values[i] === target) {
+      return i;
+    }
+  }
+  return -1;
+};
 
-let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
+let random_numbers = [6, 29, 18, 2, 72, 19, 18, 10, 37];
 
 console.log(linearSearch(18, random_numbers));
 // 2
@@ -14,17 +19,27 @@ console.log(linearSearch(9, random_numbers));
 // -1
 
 // Release 1
-console.log("Global Linear Search")
+console.log('Global Linear Search');
 let globalLinearSearch = (target, values) => {
-  //write your code here
-}
+  let index = [];
+  for (var i = 0; i < values.length; i++) {
+    if (values[i] === target) {
+      index.push(i);
+    }
+  }
+  if (index === []) {
+  	return -1;
+  } else {
+  	return index;
+  }
+};
 
-let banana_arr = "banana".split("");
+let banana_arr = 'banana'.split('');
 // ["b", "a", "n", "a", "n", "a"]
-console.log(globalLinearSearch("a", banana_arr));
+console.log(globalLinearSearch('a', banana_arr));
 // [ 1, 3, 5 ]
 
 module.exports = {
   linearSearch,
   globalLinearSearch
-}
+};
