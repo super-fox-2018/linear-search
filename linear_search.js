fa035a0c236ae6d1a51a78f358ffea35306fde5b
mario <panggihmario@gmail.com>
2018-05-01 17:26:53 +0700

    linear-search

--- linear_search.js
+++ linear_search.js
@@ -1,9 +1,22 @@
-'use strict'
+
 
 // Release 0
 console.log("Linear Search")
+
 let linearSearch = (target, values) => {
   //write your code here
+  let match = false
+  for(let i=0;i<values.length;i++){
+    if(values[i] === target){
+      match =true
+      return values.indexOf(values[i])
+    }
+    
+  }
+  if(match === false){
+    return -1
+  }
+
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
@@ -13,18 +26,3 @@ console.log(linearSearch(18, random_numbers));
 console.log(linearSearch(9, random_numbers));
 // -1
 
-// Release 1
-console.log("Global Linear Search")
-let globalLinearSearch = (target, values) => {
-  //write your code here
-}
-
-let banana_arr = "banana".split("");
-// ["b", "a", "n", "a", "n", "a"]
-console.log(globalLinearSearch("a", banana_arr));
-// [ 1, 3, 5 ]
-
-module.exports = {
-  linearSearch,
-  globalLinearSearch
-}
